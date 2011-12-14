812c89b8fa31879c60abc0af4351e242090eb8ad
Steven Xu <steven@stevenxu.ca>
2011-12-13 21:22:34 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JoinASquad::Application.config.secret_token = '10969544bb3e30d330b98b20597e9e0a9fec076550c8992a2498733533035e2b6788c51f91df82e5ba5db63e82c219f37c62774245b118c57e6f21d5eba7ed29'
