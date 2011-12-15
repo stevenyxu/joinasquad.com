981b503e4e9b89e9e373c4e17d7b957256cfcc95
Steven Xu <steven@stevenxu.ca>
2011-12-14 22:07:20 -0500

    adding erb functionality to travis reader

--- config/initializers/travis.rb
+++ config/initializers/travis.rb
@@ -1,6 +1,6 @@
 if ENV['TRAVIS']
   require 'active_support/core_ext/hash/deep_merge'
 
-  configs = YAML.load_file(Rails.root.join('config', 'database.travis.yml'))
-  ActiveRecord::Base.configurations.deep_merge!(travis_configs)
+  configs = YAML.load(ERB.new(IO.read(Rails.root.join('config', 'database.travis.yml'))).result)
+  ActiveRecord::Base.configurations.deep_merge!(configs)
 end
