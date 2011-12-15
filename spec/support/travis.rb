b795cf3a8e40dd9cb474c787ec427d806f9ec3d2
Steven Xu <steven@stevenxu.ca>
2011-12-14 22:05:28 -0500

    removing obselete spec/support/travis.rb

--- spec/support/travis.rb
+++
@@ -1,9 +0,0 @@
-if ENV['TRAVIS']
-  configs = YAML.load_file(Rails.root.join('config', 'database.travis.yml'))
-  ActiveRecord::Base.configurations = configs
-
-  db_name = ENV['DB'] || 'sqlite'
-
-  ActiveRecord::Base.establish_connection(db_name)
-  ActiveRecord::Base.default_timezone = :utc
-end
