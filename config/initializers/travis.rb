e5ce4a5db724223c75093a0e74110a877603a6b0
Steven Xu <steven@stevenxu.ca>
2011-12-14 22:21:29 -0500

    travis tweaks

--- config/initializers/travis.rb
+++ config/initializers/travis.rb
@@ -2,5 +2,5 @@ if ENV['TRAVIS']
   require 'active_support/core_ext/hash/deep_merge'
 
   configs = YAML.load(ERB.new(IO.read(Rails.root.join('config', 'database.travis.yml'))).result)
-  ActiveRecord::Base.configurations.deep_merge!(configs)
+  ActiveRecord::Base.configurations.deep_merge!(configs || {})
 end
